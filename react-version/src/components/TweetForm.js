c641d8d78e230a2f48d0e75552abaa627e133dd8
Olivia <olivia.wy.wong@gmail.com>
2021-11-02 14:37:24 -0700

    fix up styling issue with HTML import

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -3,15 +3,15 @@ import React from "react";
 function TweetForm() {
   return (
     <h1>
-      <section class="newtweet">
-        <form method="post" action="/tweets" class="newtweet__form">
+      <section className="newtweet">
+        <form method="post" action="/tweets" className="newtweet__form">
           <textarea
-            class="form__textarea"
+            className="form__textarea"
             name="text"
             placeholder="What are you humming about?"
           ></textarea>
-          <input type="submit" value="Tweet" class="form__input"></input>
-          <span class="form__counter">140</span>
+          <input type="submit" value="Tweet" className="form__input"></input>
+          <span className="form__counter">140</span>
         </form>
       </section>
     </h1>
