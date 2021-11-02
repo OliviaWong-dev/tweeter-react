28c79fa310987c742d30368b628e04301274e493
Olivia <olivia.wy.wong@gmail.com>
2021-11-02 14:19:56 -0700

    import HTML in components

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function TweetForm() {
-  return <h1>I am the tweet form component</h1>;
+  return (
+    <h1>
+      <section class="newtweet">
+        <form method="post" action="/tweets" class="newtweet__form">
+          <textarea
+            class="form__textarea"
+            name="text"
+            placeholder="What are you humming about?"
+          ></textarea>
+          <input type="submit" value="Tweet" class="form__input"></input>
+          <span class="form__counter">140</span>
+        </form>
+      </section>
+    </h1>
+  );
 }
 
 export default TweetForm;
